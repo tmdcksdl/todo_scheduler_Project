bf426f57e0fa50dffb1aed5c2b8a9b7d21e1dace
tmdcksdl <tmdcksdl99@naver.com>
2024-12-06 16:31:10 +0900

    :sparkles: Fear TodoScheduleRepository Class

---
+++ src/main/java/com/example/todoSchedulerProject/repository/TodoScheduleRepository.java
@@ -0,0 +1,15 @@
+package com.example.todoSchedulerProject.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TodoScheduleRepository implements TodoRepository{
+
+    // 속성
+
+
+    // 생성자
+
+
+    // 기능
+}

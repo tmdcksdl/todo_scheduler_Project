6bd94192fbcdbfb089804d31d5e8fd8b6a383034
tmdcksdl <tmdcksdl99@naver.com>
2024-12-06 16:36:10 +0900

    :zap: Fear todoList HashMap

--- src/main/java/com/example/todoSchedulerProject/repository/TodoScheduleRepository.java
+++ src/main/java/com/example/todoSchedulerProject/repository/TodoScheduleRepository.java
@@ -1,12 +1,16 @@
 package com.example.todoSchedulerProject.repository;
 
+import com.example.todoSchedulerProject.domain.Todo;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class TodoScheduleRepository implements TodoRepository{
 
     // 속성
-
+    private final Map<Long, Todo> todoList = new HashMap<>();
 
     // 생성자
 

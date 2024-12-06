2f5a924ab6d585b49e5aa8fc83f695634500a99c
tmdcksdl <tmdcksdl99@naver.com>
2024-12-06 16:51:50 +0900

    :zap: Fear createTodoService method

--- src/main/java/com/example/todoSchedulerProject/service/TodoScheduleService.java
+++ src/main/java/com/example/todoSchedulerProject/service/TodoScheduleService.java
@@ -1,5 +1,8 @@
 package com.example.todoSchedulerProject.service;
 
+import com.example.todoSchedulerProject.domain.Todo;
+import com.example.todoSchedulerProject.dto.TodoRequestDto;
+import com.example.todoSchedulerProject.dto.TodoResponseDto;
 import com.example.todoSchedulerProject.repository.TodoScheduleRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,4 +18,13 @@ public class TodoScheduleService implements TodoService{
     }
 
     // 기능
+    // ::: 일정 생성 서비스
+    public TodoResponseDto createTodoService(TodoRequestDto todoRequestDto) {
+
+        Todo todo = new Todo(todoRequestDto.getTitle(), todoRequestDto.getContent(), todoRequestDto.getWriter(), todoRequestDto.getPassword(), todoRequestDto.getCreated_date(), todoRequestDto.getUpdated_date());
+
+        Todo createdTodo = todoScheduleRepository.createTodo(todo);
+
+        return new TodoResponseDto(createdTodo);
+    }
 }

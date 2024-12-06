0bd775190f00b6d0e24fa01c53d72cf6519d16bd
tmdcksdl <tmdcksdl99@naver.com>
2024-12-06 15:53:00 +0900

    init Commit

---
+++ src/test/java/com/example/todoSchedulerProject/TodoSchedulerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoSchedulerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSchedulerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

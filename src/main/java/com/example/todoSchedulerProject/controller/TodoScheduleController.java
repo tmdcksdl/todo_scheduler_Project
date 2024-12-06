d84fda13297d315a9e6409eb9d3c07c9f99152a9
tmdcksdl <tmdcksdl99@naver.com>
2024-12-06 16:51:27 +0900

    :zap: Fear createTodoAPI method

--- src/main/java/com/example/todoSchedulerProject/controller/TodoScheduleController.java
+++ src/main/java/com/example/todoSchedulerProject/controller/TodoScheduleController.java
@@ -1,6 +1,12 @@
 package com.example.todoSchedulerProject.controller;
 
+import com.example.todoSchedulerProject.dto.TodoRequestDto;
+import com.example.todoSchedulerProject.dto.TodoResponseDto;
 import com.example.todoSchedulerProject.service.TodoScheduleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +23,12 @@ public class TodoScheduleController {
     }
 
     // 기능
+    // ::: 일정 생성 API
+    @PostMapping
+    public ResponseEntity<TodoResponseDto> createTodoAPI(@RequestBody TodoRequestDto todoRequestDto) {
+
+        return new ResponseEntity<>(todoScheduleService.createTodoService(todoRequestDto),HttpStatus.CREATED);
+    }
 }
 
 

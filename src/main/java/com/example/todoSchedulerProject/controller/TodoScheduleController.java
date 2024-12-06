1ae55e03c4422750d7470bbecc20ce150e07d650
tmdcksdl <tmdcksdl99@naver.com>
2024-12-06 16:28:11 +0900

    :sparkles: Fear TodoScheduleController Class

---
+++ src/main/java/com/example/todoSchedulerProject/controller/TodoScheduleController.java
@@ -0,0 +1,12 @@
+package com.example.todoSchedulerProject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoScheduleController {
+
+}
+
+

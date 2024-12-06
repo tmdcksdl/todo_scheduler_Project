0331fe471f627774aac3a75f9fc8836b314f6223
tmdcksdl <tmdcksdl99@naver.com>
2024-12-06 16:36:35 +0900

    :zap: Fear DI

--- src/main/java/com/example/todoSchedulerProject/controller/TodoScheduleController.java
+++ src/main/java/com/example/todoSchedulerProject/controller/TodoScheduleController.java
@@ -1,5 +1,6 @@
 package com.example.todoSchedulerProject.controller;
 
+import com.example.todoSchedulerProject.service.TodoScheduleService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/todos")
 public class TodoScheduleController {
 
+    // 속성
+    private final TodoScheduleService todoScheduleService;
+
+    // 생성자
+    public TodoScheduleController(TodoScheduleService todoScheduleService) {
+        this.todoScheduleService = todoScheduleService;
+    }
+
+    // 기능
 }
 
 
--- src/main/java/com/example/todoSchedulerProject/service/TodoScheduleService.java
+++ src/main/java/com/example/todoSchedulerProject/service/TodoScheduleService.java
@@ -1,15 +1,18 @@
 package com.example.todoSchedulerProject.service;
 
+import com.example.todoSchedulerProject.repository.TodoScheduleRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoScheduleService implements TodoService{
 
     // 속성
-
+    private final TodoScheduleRepository todoScheduleRepository;
 
     // 생성자
-
+    public TodoScheduleService(TodoScheduleRepository todoScheduleRepository) {
+        this.todoScheduleRepository = todoScheduleRepository;
+    }
 
     // 기능
 }

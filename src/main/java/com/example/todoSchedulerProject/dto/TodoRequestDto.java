9845334c952bbbc5b4fadb2fcf87c74c6f689a1b
tmdcksdl <tmdcksdl99@naver.com>
2024-12-06 16:26:13 +0900

    :sparkles: Fear TodoRequestDto Class

---
+++ src/main/java/com/example/todoSchedulerProject/dto/TodoRequestDto.java
@@ -0,0 +1,20 @@
+package com.example.todoSchedulerProject.dto;
+
+import lombok.Getter;
+
+@Getter
+public class TodoRequestDto {
+
+    // 속성
+    private String title;
+    private String content;
+    private String writer;
+    private String password;
+    private String created_date;
+    private String updated_date;
+
+    // 생성자
+
+
+    // 기능
+}

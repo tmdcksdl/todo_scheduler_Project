931a518f2b8ececd3d770547286ab51d2353b77a
tmdcksdl <tmdcksdl99@naver.com>
2024-12-06 16:52:03 +0900

    :zap: Fear createTodo method

--- src/main/java/com/example/todoSchedulerProject/repository/TodoScheduleRepository.java
+++ src/main/java/com/example/todoSchedulerProject/repository/TodoScheduleRepository.java
@@ -1,8 +1,10 @@
 package com.example.todoSchedulerProject.repository;
 
 import com.example.todoSchedulerProject.domain.Todo;
+import com.example.todoSchedulerProject.dto.TodoRequestDto;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,4 +18,16 @@ public class TodoScheduleRepository implements TodoRepository{
 
 
     // 기능
+    // ::: 일정 생성
+    public Todo createTodo(Todo todo) {
+
+        // todo의 id 자동 생성
+        Long todoId = todoList.isEmpty() ? 1 : Collections.max(todoList.keySet()) + 1;
+
+        todo.setId(todoId);
+
+        todoList.put(todoId, todo);
+
+        return todo;
+    }
 }

b832a8ab50d7f92c2d233555475cb12fef678a5f
tmdcksdl <tmdcksdl99@naver.com>
2024-12-06 17:25:15 +0900

    :zap: Fear searchAllTodosAPI method

--- src/main/java/com/example/todoSchedulerProject/controller/TodoScheduleController.java
+++ src/main/java/com/example/todoSchedulerProject/controller/TodoScheduleController.java
@@ -5,10 +5,9 @@ import com.example.todoSchedulerProject.dto.TodoResponseDto;
 import com.example.todoSchedulerProject.service.TodoScheduleService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/todos")
@@ -28,6 +27,13 @@ public class TodoScheduleController {
     public ResponseEntity<TodoResponseDto> createTodoAPI(@RequestBody TodoRequestDto todoRequestDto) {
 
         return new ResponseEntity<>(todoScheduleService.createTodoService(todoRequestDto),HttpStatus.CREATED);
+    }
+
+    // ::: 전체 일정 조회 API
+    @GetMapping
+    public ResponseEntity<List<TodoResponseDto>> searchAllTodosAPI(){
+
+        return new ResponseEntity<>(todoScheduleService.searchAllTodosService(), HttpStatus.OK);
     }
 }
 

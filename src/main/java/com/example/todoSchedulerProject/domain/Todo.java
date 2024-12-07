430650bbd0e04223b98ab1c1b22e901a6f37e7c2
tmdcksdl <tmdcksdl99@naver.com>
2024-12-07 15:09:23 +0900

    :zap: Fix Field & Constructor (Use LocalDateTime)

--- src/main/java/com/example/todoSchedulerProject/domain/Todo.java
+++ src/main/java/com/example/todoSchedulerProject/domain/Todo.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Getter
 @AllArgsConstructor
 public class Todo {
@@ -19,21 +22,21 @@ public class Todo {
     private String updated_date;
 
     // 생성자
-    public Todo(String title, String content, String writer, String password, String created_date, String updated_date) {
+    public Todo(String title, String content, String writer, String password) {
         this.title = title;
         this.content = content;
         this.writer = writer;
         this.password = password;
-        this.created_date = created_date;
-        this.updated_date = updated_date;
+        this.created_date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.updated_date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 
     // 기능
     // ::: Todo 수정
-    public void updateTodo(String title, String content, String writer, String updated_date) {
+    public void updateTodo(String title, String content, String writer) {
         this.title = title;
         this.content = content;
         this.writer = writer;
-        this.updated_date = updated_date;
+        this.updated_date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 }

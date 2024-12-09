c0ab581aa97a57f4a37505451e1ab5c0b5752512
tmdcksdl <tmdcksdl99@naver.com>
2024-12-09 11:43:00 +0900

    :zap: Fix deleteTodoService Method And Complete Lv2 requirements

--- src/main/java/com/example/todoSchedulerProject/service/TodoScheduleService.java
+++ src/main/java/com/example/todoSchedulerProject/service/TodoScheduleService.java
@@ -68,28 +68,25 @@ public class TodoScheduleService implements TodoService{
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "제목, 내용, 작성자명이 포함되어 있지 않습니다.");
         }
 
-        int updateRow = todoRepository.updateTodo(id, title, content, writer, password);
+        int updatedRow = todoRepository.updateTodo(id, title, content, writer, password);
 
-        if (updateRow == 0) {
+        if (updatedRow == 0) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 id입니다. id = " + id);
         }
 
         return new TodoResponseDto(todoRepository.searchTodoById(id).get());
     }
 
+    // ::: 선택 일정 삭제 서비스
     @Override
     public void deleteTodoService(Long id, String password) {
 
-//        Todo todo = todoRepository.searchTodoById(id);
-//
-//        if (todo == null) {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 id입니다. id = " + id);
-//        }
-//
-//        if (password.equals(todo.getPassword())) {
-//            todoRepository.deleteTodo(id);
-//        } else {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
-//        }
+        // 선택 일정 삭제
+        int deletedRow = todoRepository.deleteTodo(id, password);
+
+        // 삭제된 row가 0개일 때 에외처리
+        if (deletedRow == 0) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 id입니다. id = " + id);
+        }
     }
 }
